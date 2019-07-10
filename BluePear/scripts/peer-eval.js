8e4753d8d1b0ff2d0c7761932442a49330ab779b
Maucesa <pamela.maucesa@nokia.com>
2019-07-10 14:43:19 +0800

    added function for adding categories

--- BluePear/scripts/peer-eval.js
+++ BluePear/scripts/peer-eval.js
@@ -1,5 +1,33 @@
 // back-end peer eval
 
+var no_categories = 1;
+
+$(window).on('load', function(){
+     // when the latest textbox category is clicked, new textbox for category will be inserted
+     $(document).on('click', '#category', function(){
+          no_categories++;
+          document.getElementById("category").removeAttribute("id");
+          var class_name = "set" + no_categories;
+          var add_box = '<div class=' + class_name + '>\
+                                   <span class="categoryContainer">\
+                                        <label class="expandable"><img src="../images/down-arrow.png"/></label>\
+                                        <input class="category" type="text" placeholder="Type to add a category">\
+                                        <label class="delete"><img src="../images/delete-hover.png"/></label>\
+                                   </span>\
+                              <span class="questionContainer">\
+                                   <ol class="questionList">\
+                                        <li>\
+                                             <span>\
+                                                  <input class="question" type="text" placeholder="Type to add a question">\
+                                                  <label class="delete"><img src="../images/delete-hover.png"/></label>\
+                                             </span>\
+                                        </li>\
+                                   </ol>\
+                              </span>\
+                         </div>';
+          $(".append").append(add_box);
+     });
+});
 
 function show_hide_icons(id_name){
      var checkbox =  $("input:checkbox:checked").length;
@@ -14,3 +42,4 @@ function show_hide_icons(id_name){
           console.log("No/more than one item is checked :<");
      }
 }
+
