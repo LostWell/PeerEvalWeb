cc0a4c13c7295e084f9d7e64fcdc547610ffe6b6
jane-airen <maucesa.pamela@gmail.com>
2019-08-27 07:16:51 +0800

    removed feature that can add multiple teams in one questionnaire in create

--- BluePear/scripts/event_handler.js
+++ BluePear/scripts/event_handler.js
@@ -36,6 +36,7 @@ $(window).on('load', function(){
           $("#categoriesContainer").append(add_box);
      });
 
+     // to insert new textbox for questions
      $('body').on('paste keypress', '#question', function(){
           var id_category = '#' + $(this).closest('ol').attr('id');
           var name_category = (this).getAttribute('class'); // to classify later on what categories do the questions belong
@@ -52,37 +53,7 @@ $(window).on('load', function(){
           $(this).removeAttr('id');
      });
 
-     // when the latest textbox team is clicked, new textbox for team will be inserted
-     $('body').on('paste keypress', '#team', function(){
-          no_team++;
-
-          // transfer id to secure that the last added team textbox will not be deleted and on keypress in it, another textbox will appear
-          document.getElementById('team').removeAttribute('id');
-          document.getElementById('delete_team').removeAttribute('id');
-
-          var team_no = 'team' + no_team; // to classify later on what teams do the questions belong
-          var delete_team = 'delete' + no_team;
-
-          var add_box = '<div class="set">\
-                              <span class="categoryContainer">\
-                                   <label class="expandable expanded"></label>\
-                                   <input id="team" class="team" name=' + team_no + ' type="text" placeholder="Type to add a team">\
-                                   <label id="delete_team" class="delete"><img src="../images/delete-hover.png"/></label>\
-                              </span>\
-                              <span class="questionContainer">\
-                                   <ol class="itemList" id=' + team_no + '>\
-                                        <li>\
-                                             <span class="item">\
-                                                  <input class="' + team_no + ' member" id="participant" type="text" placeholder="Type to add a participant">\
-                                                  <label id=' + delete_team + ' class="delete"><img src="../images/delete-hover.png"/></label>\
-                                             </span>\
-                                        </li>\
-                                   </ol>\
-                              </span>\
-                         </div>';
-          $("#participantsContainer").append(add_box);
-     });
-
+     // to insert new textbox for participants
      $('body').on('paste keypress', '#participant', function(){
           var id_team = '#' + $(this).closest('ol').attr('id');
           var name_team = (this).getAttribute('class'); // to classify later on what teams do the participants belong
@@ -130,11 +101,4 @@ function show_hide_icons(id_name){
           icon.style.display = 'none';
           //console.log("No/more than one item is checked :<");
      }
-}
-
-
-/* PEER SIDE FUNCTIONS */
-
-function displayNames(){
-
 }
