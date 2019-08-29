a66254c6b8b4fd455096c8840bf0c2fb36b51103
LostWell <lmdl.el2@gmail.com>
2019-08-30 04:22:02 +0800

    finished every function except generate summary report

--- BluePear/php/download.php
+++ BluePear/php/download.php
@@ -7,13 +7,10 @@ $fi = new FilesystemIterator($path, FilesystemIterator::SKIP_DOTS);
 if(iterator_count($fi) != 0){
     $files = array_diff(scandir($path), array('.', '..'));
 
-    for($x=0; $x < count($files); $x++) {
-        if($x != count($files)-1){
-            $result .= $files[$x] . ",";
-        }else{
-            $result .= $files[$x];
-        }
+    foreach ($files as $value) {
+        $result .= $value . ",";
     }
+    
 } else{
     $result = "false";
 }
--- BluePear/php/uploadPeer.php
+++ BluePear/php/uploadPeer.php
@@ -4,11 +4,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.\n";
-    $uploadOk = 0;
-}
+
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.\n";
@@ -25,9 +21,11 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.\n";
     } else {
         echo "Sorry, there was an error uploading your file.\n";
     }
 }
+
+
+header("Location: ../html/home.html"); 
 ?>
